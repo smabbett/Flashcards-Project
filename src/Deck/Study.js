81d566be9db77c656ec65599d3142ea680ff4916
Sarah Abbett <sarah.abbett@gmail.com>
2021-03-15 20:50:55 -0500

    COMPLETE, updated Study.js

--- src/Deck/Study.js
+++ src/Deck/Study.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { listCards, readDeck } from '../utils/api';
+import { readDeck } from '../utils/api';
 import ErrorMessage from '../Layout/ErrorMessage';
 import classNames from '../utils/class-names';
 
 export const Study = () => {
   const { deckId } = useParams();
-  const [deck, setDeck] = useState({});
-  const [cards, setCards] = useState([]);
-
+  const [deck, setDeck] = useState({ cards: [] });
   const [error, setError] = useState(undefined);
   const [flipped, setFlipped] = useState(false);
   const [cardIndex, setCardIndex] = useState(0);
@@ -19,24 +17,20 @@ export const Study = () => {
       try {
         let data = await readDeck(deckId, abortController.signal);
         setDeck(data);
-        let cardData = await listCards(deckId, abortController.signal);
-        setCards(cardData);
-
-        // setCards(deck.cards);
       } catch (err) {
         setError(err);
       }
     }
     getDeck();
-
     return () => abortController.abort();
   }, []);
+
   if (error) {
     return <ErrorMessage error={error} />;
   }
 
   function nextCard() {
-    if (cardIndex === cards.length - 1) {
+    if (cardIndex === deck.cards.length - 1) {
       const result = window.confirm('Do you want to restart the deck?');
       if (result) {
         setCardIndex(0);
@@ -51,11 +45,6 @@ export const Study = () => {
     setFlipped((prevState) => !prevState);
   }
 
-  console.log('deck', deck);
-  console.log('cards', cards);
-  console.log('deckId', deckId);
-  console.log('deck.cards', deck.cards);
-  console.log('length', cards.length);
   if (deck) {
     return (
       <>
@@ -73,40 +62,34 @@ export const Study = () => {
           </ol>
         </nav>
         <h1>Study: {deck.name}</h1>
-
         <div className="col-sm-12">
-          {cards.length > 2 ? (
+          {deck.cards.length > 2 ? (
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">
-                  Card {cardIndex + 1} of {cards.length}
+                  Card {cardIndex + 1} of {deck.cards.length}
                 </h5>
                 <p className="card-text">
                   {!flipped
-                    ? `${cards[cardIndex].front}`
-                    : `${cards[cardIndex].back}`}
+                    ? `${deck.cards[cardIndex].front}`
+                    : `${deck.cards[cardIndex].back}`}
                 </p>
                 <button className="btn btn-secondary m-2" onClick={flipCard}>
                   Flip
                 </button>
-                <button
-                  className={classNames({
-                    'btn btn-primary': true,
-                    invisible: !flipped,
-                    visible: flipped,
-                  })}
-                  onClick={nextCard}
-                >
-                  Next
-                </button>
+                {flipped && (
+                  <button className="btn btn-primary" onClick={nextCard}>
+                    Next
+                  </button>
+                )}
               </div>
             </div>
           ) : (
             <>
               <h3>Not enough cards.</h3>
               <p>
-                You need at least 3 cards to study. There are {cards.length}{' '}
-                cards in this deck.
+                You need at least 3 cards to study. There are{' '}
+                {deck.cards.length} cards in this deck.
               </p>
               <Link
                 to={`/decks/${deck.id}/cards/new`}
